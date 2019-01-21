29fdb7fe6cc5e96d5ef275baa675b33a4f43dc5b
guanju123 <848501567@qq.com>
2019-01-21 09:46:26 +0800

    nihaoma

---
+++ scripts/js/config.js
@@ -0,0 +1,13 @@
+
+requirejs.config({
+	baseUrl : "../../scripts/require/",
+	paths : {
+		"jquery" : "jquery-1.9.0",
+		"swiper" : "swiper",
+		"common" : "common",
+		"fontscroll" : "fontscroll",
+		"animate" : "animate",
+		"my.jquery.hover" : "my.jquery.hover",
+		"require" : "require"
+	}
+});
---
+++ scripts/js/index.js
@@ -0,0 +1,47 @@
+
+<script src="../../scripts/common/animate.js" type="text/javascript" charset="utf-8"></script>
+
+<script type="text/javascript">
+		var json = [{
+			goodsId:"1213",
+			src:"img/w1.jpg",
+			name:"荣耀10  GT游戏加速 AIS手持夜景  6GB+64GB  幻影蓝全网通 移动联通电信4G 双卡双待",
+			price:"2599.00"
+		},{
+			goodsId:"1212312313",
+			src:"img/w2.jpg",
+			name:"小米 红米Note5A 移动4G+版全网通 4GB+64GB 香槟金 移动联通电信4G手机 双卡双待 拍照手机",
+			price:"999.00"
+		},{
+			goodsId:"112313213",
+			src:"img/w3.jpg",
+			name:"小米8 全面屏游戏智能手机 6GB+128GB 黑色 全网通4G 双卡双待  拍照手机",
+			price:"2999.00"
+		},{
+			goodsId:"1212321313",
+			src:"img/w4.jpg",
+			name:"荣耀9i 4GB+64GB 魅海蓝 移动联通电信4G全面屏手机 双卡双待",
+			price:"1399.00"
+		},{
+			goodsId:"12232413",
+			src:"img/w5.jpg",
+			name:"小米 红米Note5A 移动4G+版全网通 4GB+64GB 香槟金 移动联通电信4G手机 双卡双待 拍照手机",
+			price:"999.00"
+		},{
+			goodsId:"124364513",
+			src:"img/w6.jpg",
+			name:"小米8 全面屏游戏智能手机 6GB+128GB 黑色 全网通4G 双卡双待  拍照手机",
+			price:"2999.00"
+		},{
+			goodsId:"1279879813",
+			src:"img/w7.jpg",
+			name:"小米 红米Note5A 移动4G+版全网通 4GB+64GB 香槟金 移动联通电信4G手机 双卡双待 拍照手机",
+			price:"999.00"
+		},{
+			goodsId:"12asdasd13",
+			src:"img/w8.jpg",
+			name:"小米8 全面屏游戏智能手机 6GB+128GB 黑色 全网通4G 双卡双待  拍照手机",
+			price:"2999.00"
+		}];
+		
+</script>
